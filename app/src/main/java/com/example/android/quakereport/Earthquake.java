c5948042edddbf1e634c2f8cee2c85f25ee9df06
Tanmaysv <tanmaysvshetty@gmail.com>
2019-03-04 19:55:47 +0530

    Initial Uploads

---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,29 @@
+package com.example.android.quakereport;
+
+public class Earthquake {
+    private double mMagnitude;
+    private String mLocation;
+    private long mTime;
+    private String mUrl;
+
+    public Earthquake(double magnitude, String location, long time, String url){
+        this.mMagnitude = magnitude;
+        this.mLocation = location;
+        this.mTime = time;
+        this.mUrl = url;
+    }
+
+    public double getMagnitude() {
+        return mMagnitude;
+    }
+
+    public String getLocation() {
+        return mLocation;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public String getUrl() { return mUrl; }
+}
